21c7d71fbd6670f0ba58feb8e5087bdabb466e81
EBEREGIT <samsoneberenjoku@gmail.com>
2020-10-24 08:29:08 +0100

    starter code complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <div class="col align-self-center">
+          <h1 class="text-center">Email - JavaScript Contact Form</h1>
+          {/* <!-- contact form --> */}
+          <form>
+            {/* <!-- name --> */}
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input
+                type="name"
+                name="name"
+                class="form-control"
+                id="name"
+                placeholder="enter your name"
+              />
+            </div>
+
+            {/* <!-- email --> */}
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                id="email"
+                placeholder="enter your email"
+              />
+            </div>
+
+            {/* <!-- subject --> */}
+            <div class="form-group">
+              <label for="subject">Subject</label>
+              <input
+                type="text"
+                name="subject"
+                class="form-control"
+                id="subject"
+                placeholder="enter email subject"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="email_body">Message</label>
+              <textarea
+                class="form-control"
+                id="email_body"
+                rows="5"
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
